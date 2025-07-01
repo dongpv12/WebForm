0ff039f208f2bf4bf41f1e7ad5f23fbe8ad725a2
dongpv12 <phamvandong2210@gmail.com>
2025-07-01 13:14:15 +0700

    c

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ ConfigFolder = "Config";
 #endif
 
 var path = Path.Combine(Directory.GetCurrentDirectory(), ConfigFolder, "appsettings.json");
-// test git
+// test git .
 // Cấu hình đọc cấu hình từ file appsettings.json
 IConfiguration configuration = new ConfigurationBuilder()
     .AddJsonFile(path, optional: true, reloadOnChange: true)
